96ad56ea28bc358e028d6181e9476fad407b893e
Sven Mkw <sven.mkw@gmail.com>
2017-07-02 18:02:05 +0200

    verbose logging and reporting for each crawling attempt

--- index.js
+++ index.js
@@ -2,9 +2,31 @@ require('rootpath')()
 
 const fs = require('fs')
 const path = './lib/sources'
+const notify = require('lib/services/notify')
+const config = require ('conf/config.json')
 const sources = fs.readdirSync(path)
 
+const enabledPlatforms = [
+  'immoscout',
+  'immowelt',
+  'immonet',
+  'wggesucht',
+  'kleinanzeigen'
+].filter((platform) => config[platform].disabled !== true)
+
 setInterval((function exec () {
-  sources.forEach(source => require(`${path}/${source}`).run())
+  console.log(`Crawling from ${enabledPlatforms}`)
+  notify.send(
+    '\n*Crawling most recent listings from' +
+    ` ${enabledPlatforms.join(', ')}.*\n\n`)
+
+  Promise.all(sources.map(source => require(`${path}/${source}`).run()))
+    .then((results) => {
+      console.log(results)
+      const count = Array.prototype.concat(...results.filter(v => v !== false)).length
+      console.log(`Crawled ${count} new listings.`)
+      notify.send(`Finished crawling ${count} new listings.`)
+    })
+
   return exec
 }()), 60 * 60 * 1000)
